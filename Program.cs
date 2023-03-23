62f59bf9b69113055a76abc31e5aec3711ea294e
HP <saiswaroopasaranath1211@gmail.com>
2023-03-23 14:53:40 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Sorted
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var exampleIntegerSet = new SortedSet<int>() { 5, 9, 1, 2, 3, 4 };
+            //var exampleIntegerSet = new SortedSet<int>() { 1, 2, 3, 4 };
+            var exampleCharSet = new SortedSet<int>() { 'A','B','C'};
+            foreach (var example in exampleIntegerSet)
+            {
+                Console.WriteLine($"IntegerSet - {example}");
+            }
+            foreach (var example in exampleCharSet)
+            {
+                Console.WriteLine(example);
+            }
+        }
+    }
+}
